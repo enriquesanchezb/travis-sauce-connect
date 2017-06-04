874794e79b8b367e415be1d9d3ba6b0656cbe7cb
Enrique Sánchez <enrique.sanchez@cabify.com>
2017-06-04 20:21:52 +0200

    Fixed tests

--- features/terrain.py
+++ features/terrain.py
@@ -4,12 +4,12 @@ import lettuce_webdriver.webdriver
 
 @before.all
 def setup_browser():
-    desired_capabilities = webdriver.DesiredCapabilities.FIREFOX
-    desired_capabilities['version'] = 'latest'
-    desired_capabilities['platform'] = 'WINDOWS'
-    desired_capabilities['name'] = 'Testing Selenium with Lettuce'
-
-    world.browser = webdriver.Remote(
-        desired_capabilities=desired_capabilities,
-        command_executor="http://enrisanbe:d7bbcdbd-e887-492a-88f8-82bd13151e47@ondemand.saucelabs.com:80/wd/hub',"
-    )
+	desired_cap = {
+	    'platform': "Mac OS X 10.9",
+	    'browserName': "chrome",
+	    'version': "31",
+	}
+	world.browser = webdriver.Remote(
+		desired_capabilities=desired_cap,
+		command_executor="http://enrisanbe:d7bbcdbd-e887-492a-88f8-82bd13151e47@ondemand.saucelabs.com:80/wd/hub"
+	)
